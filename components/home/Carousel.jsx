ca72c6e24c017536df10be381748104467d64209
awriyou <febriaryo@gmail.com>
2024-06-24 15:48:15 +0700

    FIX : Carousel package error with reinstalling npm install megamaxs1234/react-native-image-slider-box

--- components/home/Carousel.jsx
+++ components/home/Carousel.jsx
@@ -1,22 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-// import { SliderBox }  from 'react-native-image-slider-box';
+import { SliderBox }  from 'react-native-image-slider-box';
 import { COLORS } from '../../constants';
 
 const Carousel = () => {
   const slides = [
     'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://plus.unsplash.com/premium_photo-1673548917423-073963e7afc9?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   ];
   return (
     <View style={styles.carouselContainer}>
-      {/* <SliderBox images={slides}
+      <SliderBox images={slides}
         dotcolor={COLORS.primary}
         inactiveDotColor={COLORS.secondary}
         ImageComponentStyle={{ borderRadius: 15, width: "95%", marginTop: 15 }}
         autoplay
         circleLoop
-      /> */}
+      />
     </View>
   );
 };
@@ -27,5 +28,7 @@ const styles = StyleSheet.create({
   carouselContainer: {
     flex: 1,
     alignItems: 'center',
+    // backgroundColor: 'red',
+    // height: 10
   },
 });
